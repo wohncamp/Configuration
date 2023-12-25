c59bc06a4d32d7533fa21d76c84c8f767b1ae5fe
Dajinsol Jeon <dajinsol.jeon@wohncamp.com>
2023-12-25 01:21:53 +0000

    Using anonymous repo address

--- vi/setup-vim.sh
+++ vi/setup-vim.sh
@@ -5,31 +5,31 @@ mkdir -p $PLUGIN_DIR
 if [ -d "$PLUGIN_DIR/vim-lsp" ]; then
   git -C "$PLUGIN_DIR/vim-lsp" pull
 else
-  git clone git@github.com:prabirshrestha/vim-lsp.git $PLUGIN_DIR/vim-lsp
+  git clone https://github.com/prabirshrestha/vim-lsp.git $PLUGIN_DIR/vim-lsp
 fi
 
 if [ -d "$PLUGIN_DIR/vim-lsp-settings" ]; then
   git -C "$PLUGIN_DIR/vim-lsp-settings" pull
 else
-  git clone git@github.com:mattn/vim-lsp-settings.git $PLUGIN_DIR/vim-lsp-settings
+  git clone https://github.com/mattn/vim-lsp-settings.git $PLUGIN_DIR/vim-lsp-settings
 fi
 
 if [ -d "$PLUGIN_DIR/asyncomplete.vim" ]; then
   git -C "$PLUGIN_DIR/asyncomplete.vim" pull
 else
-  git clone git@github.com:prabirshrestha/asyncomplete.vim.git $PLUGIN_DIR/asyncomplete.vim
+  git clone https://github.com/prabirshrestha/asyncomplete.vim.git $PLUGIN_DIR/asyncomplete.vim
 fi
 
 if [ -d "$PLUGIN_DIR/asyncomplete-lsp.vim" ]; then
   git -C "$PLUGIN_DIR/asyncomplete-lsp.vim" pull
 else
-  git clone git@github.com:prabirshrestha/asyncomplete-lsp.vim.git $PLUGIN_DIR/asyncomplete-lsp.vim
+  git clone https://github.com/prabirshrestha/asyncomplete-lsp.vim.git $PLUGIN_DIR/asyncomplete-lsp.vim
 fi
 
 if [ -d "$PLUGIN_DIR/nerdtree" ]; then
   git -C "$PLUGIN_DIR/nerdtree" pull
 else
-  git clone git@github.com:preservim/nerdtree.git $PLUGIN_DIR/nerdtree
+  git clone https://github.com/preservim/nerdtree.git $PLUGIN_DIR/nerdtree
 fi
 
 cp $(dirname "$0")/.vimrc ~/.vimrc
