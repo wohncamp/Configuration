e18cdc3fecd90470f6780487b92c316aaebc0a5e
Dajinsol Jeon <dajinsol.jeon@wohncamp.com>
2024-04-15 16:30:51 -0700

    Adding pyenv installation

---
+++ pyenv/setup-pyenv.sh
@@ -0,0 +1,7 @@
+brew install pyenv pyenv-virtualenv
+
+echo '' >> ~/.zshrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+
