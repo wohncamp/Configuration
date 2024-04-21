86d9031105499e4eee120de8c7e094015cd8cdee
Dajinsol Jeon <dajinsol.jeon@wohncamp.com>
2024-04-20 23:30:33 -0700

    Update pandoc setup script

--- pandoc/setup-pandoc.sh
+++ pandoc/setup-pandoc.sh
@@ -1,22 +1,32 @@
+
+if [ -z "$1" ] || ([ $1 != "html" ] && [ $1 != "pdf" ]);
+then
+    echo "Usage: ./setup-pandoc.sh [html|pdf]"
+    exit 0
+fi
+
 brew install pandoc
 
-# Setting up pandoc latec templating
-brew install --cask basictex
+if [ $1 == "html" ];
+then
+    echo "alias pandoc_html='() { pandoc -s \$1.md -o \$1.html --from markdown --to html --css https://markdowncss.github.io/\$2/css/\$2.css ; }'" >> ~/.zshrc
 
-echo '' >> ~/.zshrc
-echo 'export PATH="$PATH:/Library/TeX/texbin"' >> ~/.zshrc
+    echo "Example: pandoc_html sample [air|retro|splendor|modest]"
+fi
 
-sudo tlmgr update --self
-sudo tlmgr install elsarticle cm-super
+if [ $1 == "pdf" ];
+then
+    brew install --cask basictex
 
-mkdir -p ~/.pandoc/templates
-cp $(dirname "$0")/article.latex ~/.pandoc/templates/
+    echo '' >> ~/.zshrc
+    echo 'export PATH="$PATH:/Library/TeX/texbin"' >> ~/.zshrc
 
-echo "alias pandoc_pdf='() { pandoc -s \$1.md -o \$1.pdf --from markdown --to pdf --template article ; }'" >> ~/.zshrc
-echo "Example: pandoc_pdf sample"
-
-# Setting up pandoc html templating
-
-echo "alias pandoc_html='() { pandoc -s \$1.md -o \$1.html --from markdown --to html --css https://markdowncss.github.io/\$2/css/\$2.css ; }'" >> ~/.zshrc
-
-echo "Example: pandoc_html sample [air|retro|splendor|modest]"
+	sudo tlmgr update --self
+	sudo tlmgr install elsarticle cm-super
+	
+	mkdir -p ~/.pandoc/templates
+	cp $(dirname "$0")/article.latex ~/.pandoc/templates/
+	
+	echo "alias pandoc_pdf='() { pandoc -s \$1.md -o \$1.pdf --from markdown --to pdf --template article ; }'" >> ~/.zshrc
+	echo "Example: pandoc_pdf sample"
+fi
